ef81888d3863c539961d0b0fcab2fe9b5822b141
Talha Ghauri <talhaghauridev@gmail.com>
2025-09-14 17:27:05 +0500

    update

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ const server = app.listen(env.PORT, async () => {
    console.log(`Server is running on http://localhost:${env.PORT} in ${env.NODE_ENV.toUpperCase()} mode`);
 });
 
-server.keepAliveTimeout = 300000;
-server.headersTimeout = 310000;
-server.timeout = 300000;
-
+server.keepAliveTimeout = 600000;
+server.headersTimeout = 610000;
+server.timeout = 600000;
 export default app;
